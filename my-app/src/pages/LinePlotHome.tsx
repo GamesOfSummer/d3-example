35f5d3b934548f5a1c618abdf5d3d921a6033a50
Summer T <summer@summertime-development.com>
2024-11-04 14:26:12 -0600

    Setup Next.js for routing

---
+++ my-app/src/pages/LinePlotHome.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import * as d3 from "d3";
+import LinePlot from "./LinePlot";
+
+export default function LinePlotHome() {
+  const [data, setData] = useState(() => d3.ticks(-2, 2, 200).map(Math.sin));
+
+  function onMouseMove(event) {
+    const [x, y] = d3.pointer(event);
+    setData(data.slice(-200).concat(Math.atan2(x, y)));
+  }
+
+  return (
+    <div onMouseMove={onMouseMove}>
+      <LinePlot data={data} />
+    </div>
+  );
+}
