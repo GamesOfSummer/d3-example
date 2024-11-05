18c9ab70f04b81f13b1ed22f9ff8fc0bad3b1038
Summer T <summer@summertime-development.com>
2024-11-05 14:20:52 -0600

    Got something kinda showing....

--- my-app/src/pages/SunBurst.tsx
+++ my-app/src/pages/SunBurst.tsx
@@ -2,11 +2,9 @@ import React, { useEffect, useRef, useState } from "react";
 
 import * as d3 from "d3";
 
-import sunburstData from "./sunburst.json";
+import data from "./sunburst.json";
 
 export default function SunBurst() {
-  // const [data, setData] = useState(() => d3.ticks(-2, 2, 200).map(Math.sin));
-
   const containerRef = useRef(null);
   useEffect(() => {
     drawChart();
@@ -16,7 +14,7 @@ export default function SunBurst() {
     // Specify the chart’s dimensions.
     const width = 928;
     const height = width;
-    const radius = width / 6;
+
     const tau = 2 * Math.PI;
     const maxValue = 100;
     const slice = 80;
@@ -26,57 +24,69 @@ export default function SunBurst() {
       startAngle = 0,
       cornerRadius = 40;
 
-    d3.select("svg").remove();
+    //d3.select("svg").remove();
 
-    // Compute the layout.
-    const hierarchy = d3.hierarchy(sunburstData);
-
-    console.log(hierarchy);
-
-    const root = d3.partition().size([2 * Math.PI, hierarchy.height + 1])(
-      hierarchy
+    // Specify the chart’s colors and approximate radius (it will be adjusted at the end).
+    const color = d3.scaleOrdinal(
+      d3.quantize(d3.interpolateRainbow, data.children.length + 1)
     );
+    const radius = 928 / 2;
 
-    console.log("testing debug output");
-    console.log(root);
+    const sumTotalValue = d3
+      .hierarchy(data)
+      .copy()
+      .sum((d) => d.value);
 
-    // An arc will be created
-    // Create the arc generator.
+    console.log("-- sumTotalValue");
+    console.log(sumTotalValue.value);
+    console.log(sumTotalValue);
+
+    // Prepare the layout.
+    const partition = (data) =>
+      d3.partition().size([2 * Math.PI, radius])(
+        d3
+          .hierarchy(data)
+          .count()
+          .sum((d) => d.value)
+      );
+
+    console.log("-- partition");
+    console.log(partition);
+
     const arc = d3
       .arc()
       .startAngle((d) => d.x0)
       .endAngle((d) => d.x1)
       .padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
-      .padRadius(radius * 1.5)
-      .innerRadius((d) => d.y0 * radius)
-      .outerRadius((d) => Math.max(d.y0 * radius, d.y1 * radius - 1));
+      .padRadius(radius / 2)
+      .innerRadius((d) => d.y0)
+      .outerRadius((d) => d.y1 - 1);
+
+    console.log("-- arc");
+    console.log(arc);
+
+    const root = partition(data);
+
+    console.log("-- root");
+    console.log(root);
 
     const svg = d3
       .select(containerRef.current)
-      .attr("viewBox", [-width / 2, -height / 2, width, width])
-      .style("font", "10px sans-serif");
-
-    // Create the color scale.
-    const color = d3.scaleOrdinal(
-      d3.quantize(d3.interpolateRainbow, sunburstData.length + 1)
-    );
-
-    // Append the arcs.
-    const path = svg
+      .append("svg")
+      .attr("height", "60%")
+      .attr("width", "100%")
+      .attr("viewBox", `0 0 ${width} ${height}`)
       .append("g")
-      .selectAll("path")
-      .data(root.descendants().slice(1))
-      .join("path")
-      .attr("fill", (d) => {
-        while (d.depth > 1) d = d.parent;
-        return color(d.data.name);
-      })
-      .attr("fill-opacity", (d) =>
-        arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0
-      )
-      .attr("pointer-events", (d) => (arcVisible(d.current) ? "auto" : "none"))
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-      .attr("d", (d) => arc(d.current));
+    svg
+      .append("g")
+      .attr("fill-opacity", 0.6)
+      .selectAll("path")
+      .data(root.descendants().filter((d) => d.depth))
+      .join("path")
+      .attr("d", arc)
+      .append("title");
 
     svg
       .append("text")
@@ -84,6 +94,20 @@ export default function SunBurst() {
       .text(`Movies`)
       .style("font-size", "3.2em")
       .style("fill", "#A9BF51");
+  }
+
+  function arcVisible(d) {
+    return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+  }
+
+  function labelVisible(d) {
+    return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+  }
+
+  function labelTransform(d) {
+    const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
+    const y = ((d.y0 + d.y1) / 2) * radius;
+    return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
   }
 
   return (
