9cb5b81160fcfc6bbf0a1d90b398ebfed9a1f9c3
Summer T <summer@summertime-development.com>
2024-11-06 14:25:07 -0600

    Move towards a pure sundial

--- my-app/src/pages/SunBurst.tsx
+++ my-app/src/pages/SunBurst.tsx
@@ -12,74 +12,67 @@ export default function SunBurst() {
 
   function drawChart() {
     // Specify the chart’s dimensions.
-    const width = 928;
-    const height = width;
+    const width = 558;
+    const height = 900;
+    const radius = width / 6;
 
-    const tau = 2 * Math.PI;
-    const maxValue = 100;
-    const slice = 80;
+    // Compute the layout.
+    const hierarchy = d3
+      .hierarchy(data)
+      .sum((d) => d.value)
+      .sort((a, b) => b.value - a.value);
 
-    const innerRadius = 90,
-      outerRadius = 120,
-      startAngle = 0,
-      cornerRadius = 40;
+    const root = d3.partition().size([2 * Math.PI, hierarchy.height + 1])(
+      hierarchy
+    );
 
-    const radius = 928 / 2;
+    root.each((d) => (d.current = d));
 
-    // Prepare the layout.
-    const partition = (data) =>
-      d3.partition().size([2 * Math.PI, radius])(
-        d3
-          .hierarchy(data)
-          .count()
-          .sum((d) => d.value)
-      );
-
-    console.log("-- partition");
-    console.log(partition);
-
+    // Create the arc generator.
     const arc = d3
       .arc()
       .startAngle((d) => d.x0)
       .endAngle((d) => d.x1)
       .padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
-      .padRadius(radius / 2)
-      .innerRadius((d) => d.y0)
-      .outerRadius((d) => d.y1 - 1);
-
-    console.log("-- arc");
-    console.log(arc);
-
-    const root = partition(data);
-
-    console.log("-- root");
-    console.log(root);
+      .padRadius(radius * 2)
+      .innerRadius((d) => d.y0 * radius)
+      .outerRadius((d) => Math.max(d.y0 * radius, d.y1 * radius - 1));
 
     const svg = d3
       .select(containerRef.current)
       .append("svg")
-      .attr("height", "60%")
-      .attr("width", "80%")
-      .attr("viewBox", `0 0 ${width} ${height}`)
+      .attr("height", "1200")
+      .attr("width", "800")
+      .attr("viewBox", `0 0 800 1200`)
       .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+      .attr("transform", "translate(" + 400 + "," + 400 + ")");
 
     const color = d3
       .scaleSequential(d3.interpolateYlGn)
       .domain(d3.extent(root.descendants(), (d) => d.depth));
 
+    //create arcs
+    // svg
+    //   .append("g")
+    //   .attr("fill-opacity", 0.6)
+    //   .selectAll("path")
+    //   .data(root.descendants().filter((d) => d.depth))
+    //   .join("path")
+    //   .attr("d", arc)
+    //   .attr("fill", (d) => color(d.depth))
+    //   .append("title")
+    //   .text(
+    //     (d) => `Color: ${color(d.depth)}, Depth: ${d.depth}, Value: ${d.value}`
+    //   );
+
+    // Append the arcs.
     svg
       .append("g")
-      .attr("fill-opacity", 0.6)
       .selectAll("path")
       .data(root.descendants().filter((d) => d.depth))
       .join("path")
-      .attr("d", arc)
       .attr("fill", (d) => color(d.depth))
-      .append("title")
-      .text(
-        (d) => `Color: ${color(d.depth)}, Depth: ${d.depth}, Value: ${d.value}`
-      );
+      .attr("d", arc);
 
     //labels
     svg
@@ -100,7 +93,7 @@ export default function SunBurst() {
           .text((d) => d.data.name)
       );
 
-    // B text
+    // Letter grade text
     svg
       .append("text")
       .attr("text-anchor", "middle")
@@ -108,20 +101,6 @@ export default function SunBurst() {
       .text(`B+`)
       .style("font-size", "5.2em")
       .style("fill", "#A9BF51");
-  }
-
-  function arcVisible(d) {
-    return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
-  }
-
-  function labelVisible(d) {
-    return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
-  }
-
-  function labelTransform(d) {
-    const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
-    const y = ((d.y0 + d.y1) / 2) * radius;
-    return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
   }
 
   return (
